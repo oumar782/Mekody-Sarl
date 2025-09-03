ce9fcd95b65144ae470a8ac7e26a7c2d9fb1aa3d
Oumar Diane <oumardiane399@gmail.com>
2025-09-04 00:54:29 +0200

    first commit2

--- src/Page/Developpement.jsx
+++ src/Page/Developpement.jsx
@@ -26,7 +26,7 @@ import {
   Phone,
   MessageCircle
 } from 'lucide-react';
-import './developpement.css'
+import '../style/developpement.css'
 import Header from '../composant/Header';
 import Footer from '../composant/Footer';
 
